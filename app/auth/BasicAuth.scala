3957c1a4f5648d113eb113f37a2c9dbe37baefac
Vladimir Aleksandrov <invis87@gmail.com>
2015-10-31 20:16:50 +0300

    remove offcast code

--- app/auth/BasicAuth.scala
+++ app/auth/BasicAuth.scala
@@ -3,43 +3,12 @@ package auth
 import auth.source.AuthSource
 import play.api.mvc._
 
-import scala.concurrent.Future
-
 object BasicAuth extends Controller {
 
-  //todo: remove when finish project
-//  implicit def auth[A](username: String, password: String, action: Action[A]): Action[A] = Action.async(action.parser) { request =>
-//    val authHead = request.headers.get("Authorization")
-//    if(authHead.isDefined) {
-//      val encoded = authHead.get.split(" ").drop(1).headOption
-//      val x = encoded.filter(filterEncodedLoginPass(_, username, password))
-//
-//      if(x.isDefined){
-//        action(request)
-//      } else {
-//        Future.successful(Unauthorized.withHeaders("WWW-Authenticate" -> """Basic realm="Secured""""))
-//      }
-//
-//    } else {
-//      Future.successful(Unauthorized.withHeaders("WWW-Authenticate" -> """Basic realm="Secured""""))
-//    }
-//  }
-
-  implicit def authNew(block: => Result, source: AuthSource): Action[AnyContent] = Action.async(BodyParsers.parse.default) { request =>
+  implicit def authNew(block: => Result, source: AuthSource): Action[AnyContent] = Action(BodyParsers.parse.default) { request =>
     val authHead = request.headers.get("Authorization")
-    if(authHead.isDefined) {
-      val encoded = authHead.get.split(" ").drop(1).headOption
-      val x = encoded.filter(filterEncodedLoginPass(_, source))
-
-      if(x.isDefined){
-        Future.successful(block)
-      } else {
-        Future.successful(Unauthorized.withHeaders("WWW-Authenticate" -> """Basic realm="Secured""""))
-      }
-
-    } else {
-      Future.successful(Unauthorized.withHeaders("WWW-Authenticate" -> """Basic realm="Secured""""))
-    }
+    val authOption = authHead.flatMap(str => str.split(" ").drop(1).headOption.filter(filterEncodedLoginPass(_, source)))
+    authOption.map(_ => block).getOrElse(Unauthorized.withHeaders("WWW-Authenticate" -> """Basic realm="Secured""""))
   }
 
   def filterEncodedLoginPass(str: String, source: AuthSource): Boolean = {
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,50 +3,11 @@ package controllers
 import auth.SecuredAction
 
 import play.api.mvc._
-import scala.concurrent.Future
 import auth.BasicAuth._
 
 class Application extends Controller {
 
-//  def index = Secured("vov", "vov") (
-//    Action {
-//      Ok(views.html.index("Your new application is ready."))
-//    })
-
-//  def index = SecuredAction.xz(
-//    Action {
-//    Ok(views.html.index("Your new application is ready."))
-//  })
-
-
   def index = SecuredAction {
     Ok(views.html.index("Your new application is ready."))
-  }
-
-  def Secured[A](username: String, password: String)(action: Action[A]) = Action.async(action.parser) { request =>
-    val authHead = request.headers.get("Authorization")
-    if(authHead.isDefined) {
-      println(authHead)
-      //Unauthorized.withHeaders("WWW-Authenticate" -> """Basic realm="Secured"""")
-
-      val encoded = authHead.get.split(" ").drop(1).headOption
-      val x = encoded.filter(filterEncodedLoginPass(_, username, password))
-
-      if(x.isDefined){
-        action(request)
-      } else {
-        Future.successful(Unauthorized.withHeaders("WWW-Authenticate" -> """Basic realm="Secured""""))
-      }
-
-    } else {
-      Future.successful(Unauthorized.withHeaders("WWW-Authenticate" -> """Basic realm="Secured""""))
-    }
-  }
-
-  def filterEncodedLoginPass(str: String, username: String, password: String): Boolean = {
-    new String(org.apache.commons.codec.binary.Base64.decodeBase64(str.getBytes)).split(":").toList match {
-      case u :: p :: Nil if u == username && password == p => true
-      case _ => false
-    }
   }
 }
